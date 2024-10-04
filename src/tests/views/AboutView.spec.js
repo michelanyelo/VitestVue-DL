2269185ec26bae67c2f69c2cad4b74168b0ae319
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-10-04 18:57:50 -0300

    M7D11 desafio: testing AboutView

---
+++ src/tests/views/AboutView.spec.js
@@ -0,0 +1,33 @@
+import { describe, expect, it } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import AboutView from '@/views/AboutView.vue'
+import App from '@/App.vue'
+import router from '@/router'
+
+describe("Renderiza AboutView después de navegar a la ruta 'About'", async () => {
+    const routerPrueba = createRouter({
+        history: createWebHistory(),
+        routes: [
+            {
+                path: '/about',
+                name: 'about',
+                component: AboutView
+            }
+        ]
+    })
+
+    it("Navega y renderiza la vista correspondiente", async () => {
+        routerPrueba.push({ name: 'about' })
+        await routerPrueba.isReady()
+
+        const wrapper = mount(App, {
+            global: {
+                plugins: [router]
+            }
+        })
+
+        expect(wrapper.findComponent(AboutView).exists).toBeTruthy
+    })
+})
