9e0939468b707f79f7764c223a89bd0d522c4e7c
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-10-04 19:03:04 -0300

    M7D11 desafio: testing ContactView

---
+++ src/tests/views/ContactView.spec.js
@@ -0,0 +1,34 @@
+import { describe, expect, it } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import ContactView from '@/views/ContactView.vue'
+import App from '@/App.vue'
+import router from '@/router'
+
+
+describe("Renderiza ContactView después de navegar a la ruta 'Contact'", async () => {
+    const routerPrueba = createRouter({
+        history: createWebHistory(),
+        routes: [
+            {
+                path: '/contact',
+                name: 'contact',
+                component: ContactView
+            },
+        ]
+    })
+
+    it("Navega y renderiza la vista correspondiente", async () => {
+        routerPrueba.push({ name: 'contact' })
+        await routerPrueba.isReady()
+
+        const wrapper = mount(App, {
+            global: {
+                plugins: [router],
+            }
+        })
+
+        expect(wrapper.findComponent(ContactView).exists).toBeTruthy
+    })
+})
