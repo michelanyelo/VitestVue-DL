47176719344fe83b71be19dfff756ea125c35d0b
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-10-04 18:46:25 -0300

    M7D11 desafio: Refactor App.vue, add Posts route and view, and update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <PostsView />
+  </div>
 </template>
+
+<script>
+import PostsView from './PostsView.vue';
+
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    PostsView
+}
+}
+</script>
